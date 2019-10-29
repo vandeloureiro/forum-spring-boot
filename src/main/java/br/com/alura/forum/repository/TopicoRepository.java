bb41550410fa0f3359cdda33fe09275fecb17584
Vanderlei <desenvolvedor8@f5promotora.com.br>
2019-10-29 09:45:37 -0300

    Primeiro commit

---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,20 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.alura.forum.modelo.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long>{
+
+	// Curso é a antidade e concatena com Nome que é atributo de Curso
+	List<Topico> findByCurso_Nome(String nomeCurso);
+	
+	// Uma variação do filtro de pesquisa
+	
+	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
+	List<Topico> carregaPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
+}
