e90b3a888217c146f552251dc2a4d6e23a83ca24
Vanderlei <desenvolvedor8@f5promotora.com.br>
2019-11-19 11:31:51 -0300

    paginação no método listar tópicos

--- src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+
 import br.com.alura.forum.modelo.Topico;
 
 public class TopicoDto {
@@ -20,8 +22,11 @@ public class TopicoDto {
 		this.dataCriacao = topico.getDataCriacao();
 	}
 	
-	public static List<TopicoDto> converter(List<Topico> topicos) {
-		return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());	
+	public static Page<TopicoDto> converter(Page<Topico> topicos) {
+		// conversão quando não havia paginação com Page, usando apenas List
+		// return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());	
+		
+		return topicos.map(TopicoDto::new);
 	}
 	
 	public Long getId() {
--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -2,6 +2,8 @@ package br.com.alura.forum.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,5 +18,5 @@ public interface TopicoRepository extends JpaRepository<Topico, Long>{
 	// Uma variação do filtro de pesquisa
 	
 	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
-	List<Topico> carregaPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
+	Page<Topico> carregaPorNomeDoCurso(@Param("nomeCurso") String nomeCurso, Pageable paginacao);
 }
